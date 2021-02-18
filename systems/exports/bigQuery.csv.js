9cc2ee45b3cb5d784f5e060da7cd669ffedf097f
Faraz <faraz.faraji@gmail.com>
2021-02-18 16:49:54 +0300

    update git ignore fixing path of bigquery credential file

--- systems/exports/bigQuery.csv.js
+++ systems/exports/bigQuery.csv.js
@@ -5,49 +5,48 @@ const CSV = require("./csv.export");
 
 class bigQueryClass extends CSV {
 
-    constructor(data){
+    constructor(data) {
         super(data);
         this.bigQuery = new BigQuery({
             projectId: 'sigmacdr',
-            keyFilename: '/Users/faraz.faraji/PhpstormProjects/zoho-boilerplate/assets/sigmaCdr-bigquery.json'//process.env.BQ_PATH
+            keyFilename: process.env.BQ_PATH
         });
     }
 
-    async getInfo(dataset,table){
+    async getInfo(dataset, table) {
         const data = this.bigQuery.dataset(dataset).table(table);
         return await data.getMetadata();
     }
 
-    async upload(dataset,table){
+    async upload(dataset, table) {
         const randomNumber = Math.floor(Math.random() * (1000000 - 1) + 1);
         await this.validate().save("_temp_" + randomNumber);
         const firstKey = Object.keys(this._data[0]).length;
-        const bigQueryMetadata = await this.getInfo(dataset,table);
+        const bigQueryMetadata = await this.getInfo(dataset, table);
 
-        if(firstKey!==Object.keys(bigQueryMetadata[0].schema.fields).length)
+        if (firstKey !== Object.keys(bigQueryMetadata[0].schema.fields).length)
             throw new Error("schema is not same");
 
         const metadata = {
             sourceFormat: 'CSV',
             skipLeadingRows: 1,
             autodetect: false,
-            writeDisposition:'WRITE_APPEND'
+            writeDisposition: 'WRITE_APPEND'
         };
-        await this.bigQuery.dataset(dataset).table(table).load(filename,metadata);
+        await this.bigQuery.dataset(dataset).table(table).load(filename, metadata);
         fs.unlinkSync("_temp_" + randomNumber);
     }
 
-    async createTable(dataset,tableName,fields,types){
-        try{
+    async createTable(dataset, tableName, fields, types) {
+        try {
             let deleteTable = `drop table ${dataset}.${tableName}`;
             await this.bigQuery.query(deleteTable);
-        }
-        catch (e) {
+        } catch (e) {
 
         }
         let columns = "";
-        for(let i = 0;i<fields.length;i++){
-            if(types[fields[i]])
+        for (let i = 0; i < fields.length; i++) {
+            if (types[fields[i]])
                 columns += fields[i] + " " + types[fields[i]] + ",";
             else
                 columns += fields[i] + " STRING,"
@@ -57,11 +56,12 @@ class bigQueryClass extends CSV {
     }
 }
 
-async function test(){
-    const r = new bigQueryClass([{"SALAM":"dsa"}]);
+async function test() {
+    const r = new bigQueryClass([{"SALAM": "dsa"}]);
     await r.validate().save("test.csv");
-    const rs = await r.upload("5G_CDRS","iran-numbers");
+    const rs = await r.upload("5G_CDRS", "iran-numbers");
     console.log(rs);
 }
+
 test();
 module.exports = bigQueryClass;
