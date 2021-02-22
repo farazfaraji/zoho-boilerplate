308361f2ca54fc269c140099f959f8560f0abb70
Faraz <faraz.faraji@gmail.com>
2021-02-22 18:05:53 +0300

    - add scheduler system to DI

--- di.js
+++ di.js
@@ -1,17 +1,20 @@
 const Redis = require("async-redis");
 const orchestlySystem = require("./systems/services/orchestly.service");
+const Scheduler = require("./systems/services/scheduler.service");
 
 class DI {
-    constructor(){
-        this.orchestly = new orchestlySystem(this,process.env.client_id,process.env.client_secret,process.env.refresh_token);
+    constructor() {
+        this.orchestly = new orchestlySystem(this, process.env.client_id, process.env.client_secret, process.env.refresh_token);
+        this.scheduler = new Scheduler(this, {report:["sms"]});
         this.redis = null;
     }
 
-    async connectToRedis(){
+    async connectToRedis() {
         this.redis = Redis.createClient(`redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`);
     }
 
 }
+
 let di = new DI();
 
 module.exports = di;
