28a2eb1b3ba0bff0e5d5096894252076e7870b7d
Faraz <faraz.faraji@gmail.com>
2021-02-26 12:20:19 +0300

    zoho module added

---
+++ systems/services/zoho.service.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const ZohoAuth = require("./zoho_auth.service");
+
+const di = require("../../di");
+
+class Zoho extends ZohoAuth {
+    constructor(di, _client_id, _client_secret, _refresh_token) {
+        super(di, _client_id, _client_secret, _refresh_token)
+    }
+
+    async search(moduleName, criteria) {
+        try {
+            return await this.customRequest(`https://www.zohoapis.com/crm/v2/${moduleName}/search?criteria=${criteria}`, "GET",data);
+        }catch (e) {
+            console.error(e);
+        }
+
+    }
+
+    async update(moduleName,record_id,data ) {
+        try {
+            return await this.customRequest(`https://www.zohoapis.com/crm/v2/${moduleName}/${record_id}`, "PUT",data);
+        }catch (e) {
+            console.error(e);
+        }
+
+    }
+
+
+}
+
+module.exports = Zoho;
